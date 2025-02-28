a206fe71bf9ea02b779cce5b0c6c06b840ef4edd
it-nag <ramon@ptnag.com>
2025-02-28 07:52:33 +0700

    adjusting

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="icon" type="image/x-icon" href="{{ asset('images/finishline.ico') }}">
-    <title>NDS PACKING LINE</title>
+    <title>NDS FINISH LINE</title>
 
     @include('layouts.link')
 
